5aef5e6f8389c8cf8195cb83917fade8777a7582
Trần Nguyễn Quy <kekkei2002@gmail.com>
2023-09-13 13:02:59 +0700

    update rerender

--- src/containers/Food/AddFoodFormContainer/AddFoodFormContainer.tsx
+++ src/containers/Food/AddFoodFormContainer/AddFoodFormContainer.tsx
@@ -10,16 +10,46 @@ const AddFoodFormContainer = () => {
 
   const dispatch = useAppDispatch();
 
+<<<<<<< Updated upstream
   const { register, handleSubmit } = useForm({
+=======
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+    trigger,
+  } = useForm({
+>>>>>>> Stashed changes
     resolver: ajvResolver(AddFoodFormSchema as any),
   });
 
   const onSubmit = (values: any) => {
+<<<<<<< Updated upstream
     dispatch(addFoodItem(values));
+=======
+    const { type, ...rest } = values;
+    if (type === "add") {
+      dispatch(addFoodItem(rest));
+    }
+  };
+
+  const handleChange = (e: any) => {
+    if (!trigger()) return;
+    const { type, ...rest } = getValues();
+    if (type === "edit") {
+      dispatch(editFoodItem(rest));
+    }
+>>>>>>> Stashed changes
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-1">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      onChange={handleChange}
+      className="flex flex-col gap-1"
+    >
       <div>Category</div>
       <select
         {...register("categoryId", {
@@ -51,6 +81,20 @@ const AddFoodFormContainer = () => {
           valueAsNumber: true,
         })}
       />
+<<<<<<< Updated upstream
+=======
+      <div className="flex gap-3">
+        <label htmlFor="add">
+          <input {...register("type")} type="radio" value="add" id="add" />
+          Add
+        </label>
+        <label htmlFor="edit">
+          <input {...register("type")} type="radio" value="edit" id="edit" />
+          Edit
+        </label>
+      </div>
+      <div className="text-red-400">{(errors as any)?.type?.message}</div>
+>>>>>>> Stashed changes
       <button type="submit" className="bg-cyan-300 px-2 py-1 rounded-md mt-2">
         Submit
       </button>
--- src/containers/Food/FoodItemListContainer/FoodItemListContainer.tsx
+++ src/containers/Food/FoodItemListContainer/FoodItemListContainer.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import FoodCard from "../../../components/Cards/Food";
+<<<<<<< Updated upstream
 import { useAppSelector } from "../../../redux/store";
 import { FoodCategory, FoodItem } from "../../../types/Food";
 import React from "react";
@@ -29,12 +30,33 @@ const CategoryFoodItemList = React.memo(
   (prevProps, nextProps) =>
     JSON.stringify(prevProps.items) === JSON.stringify(nextProps.items)
 );
+=======
+import { selectFoodItemId } from "../../../redux/foods/foods.slice";
+import { useAppDispatch, useAppSelector } from "../../../redux/store";
+import { useCallback } from "react";
+import { FoodItem } from "../../../types/Food";
+
+type FoodItemWrapperProps = FoodItem & {
+  onClick: (id: number) => void;
+};
+
+const FoodItemWrapper = (props: FoodItemWrapperProps) => {
+  const currentFoodItemId = useAppSelector(
+    (state) => state.foods.currentFoodItemId
+  );
+
+  return (
+    <FoodCard.Preview {...props} isSelected={currentFoodItemId === props.id} />
+  );
+};
+>>>>>>> Stashed changes
 
 type FoodItemListContainerProps = {
   className?: string;
 };
 
 const FoodItemListContainer = ({ className }: FoodItemListContainerProps) => {
+<<<<<<< Updated upstream
   const { items, categories } = useAppSelector((state) => state.foods);
 
   return (
@@ -48,6 +70,42 @@ const FoodItemListContainer = ({ className }: FoodItemListContainerProps) => {
           />
         );
       })}
+=======
+  const categories = useAppSelector((state) => state.foods.categories);
+  const items = useAppSelector((state) => state.foods.items);
+  const currentCategoryId = useAppSelector(
+    (state) => state.foods.currentCategoryId
+  );
+
+  const dispatch = useAppDispatch();
+
+  const handleFoodItemClick = useCallback(
+    (id: number) => dispatch(selectFoodItemId(id)),
+    [dispatch]
+  );
+  console.log("FoodItemListContainer");
+
+  return (
+    <div className={`flex flex-col gap-2 ${className}`}>
+      {categories.map((category) => (
+        <div
+          className={`flex flex-col gap-2 ${
+            category.id !== currentCategoryId && "hidden"
+          }`}
+          key={category.id}
+        >
+          {items
+            .filter((item) => item.categoryId === category.id)
+            .map((item) => (
+              <FoodItemWrapper
+                {...item}
+                onClick={handleFoodItemClick}
+                key={item.id}
+              />
+            ))}
+        </div>
+      ))}
+>>>>>>> Stashed changes
     </div>
   );
 };
