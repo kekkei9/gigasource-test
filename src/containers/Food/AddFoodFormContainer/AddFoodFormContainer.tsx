e47279178a299a3a5a59601d42de52c2f9783c0f
Trần Nguyễn Quy <kekkei2002@gmail.com>
2023-09-13 11:54:26 +0700

    Feat/fix functionality (#2)
    * temporary update style
    
    * update full flow

--- src/containers/Food/AddFoodFormContainer/AddFoodFormContainer.tsx
+++ src/containers/Food/AddFoodFormContainer/AddFoodFormContainer.tsx
@@ -2,20 +2,47 @@ import { useForm } from "react-hook-form";
 import { ajvResolver } from "@hookform/resolvers/ajv";
 import { AddFoodFormSchema } from "./validator";
 import { useAppDispatch, useAppSelector } from "../../../redux/store";
-import { addFoodItem } from "../../../redux/foods/foods.slice";
+import { addFoodItem, editFoodItem } from "../../../redux/foods/foods.slice";
+import { useCallback, useEffect, useMemo } from "react";
 
 const AddFoodFormContainer = () => {
   const categories = useAppSelector((state) => state.foods.categories);
+  const items = useAppSelector((state) => state.foods.items);
+  const currentFoodItemId = useAppSelector(
+    (state) => state.foods.currentFoodItemId
+  );
   console.log("AddFoodFormContainer");
 
   const dispatch = useAppDispatch();
 
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, reset } = useForm({
     resolver: ajvResolver(AddFoodFormSchema as any),
   });
 
+  useEffect(() => {
+    reset(items.find((item) => item.id === currentFoodItemId));
+  }, [currentFoodItemId, reset]);
+
+  const categoryOptions = useMemo(
+    () => (
+      <>
+        {categories.map(({ id, name }) => (
+          <option value={id} key={id}>
+            {name}
+          </option>
+        ))}
+      </>
+    ),
+    [categories]
+  );
+
   const onSubmit = (values: any) => {
-    dispatch(addFoodItem(values));
+    const { type, ...rest } = values;
+    if (type === "add") {
+      dispatch(addFoodItem(rest));
+      return;
+    }
+    dispatch(editFoodItem(rest));
   };
 
   return (
@@ -26,11 +53,7 @@ const AddFoodFormContainer = () => {
           valueAsNumber: true,
         })}
       >
-        {categories.map(({ id, name }) => (
-          <option value={id} key={id}>
-            {name}
-          </option>
-        ))}
+        {categoryOptions}
       </select>
 
       <div>ID</div>
@@ -44,15 +67,24 @@ const AddFoodFormContainer = () => {
 
       <input {...register("name")} />
       <div>Price</div>
-
       <input
-        type="number"
+        type="price"
         {...register("price", {
           valueAsNumber: true,
         })}
       />
+      <div className="flex gap-3">
+        <label htmlFor="add">
+          <input {...register("type")} type="radio" value="add" id="add" />
+          Add
+        </label>
+        <label htmlFor="edit">
+          <input {...register("type")} type="radio" value="edit" id="edit" />
+          Edit
+        </label>
+      </div>
       <button type="submit" className="bg-cyan-300 px-2 py-1 rounded-md mt-2">
-        Submit
+        Save/Add
       </button>
     </form>
   );
